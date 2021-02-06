b54e0e76697d140bf3351335c7f5e41b047e5203
Matthew Felton <fel19007@byui.edu>
2021-02-06 01:18:18 -0600

    added summary/hero/contactus

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,39 @@
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("disabled");
+    document.getElementById("primaryOpen").classList.toggle("disabled");
+    document.getElementById("primaryClose").classList.toggle("disabled");
+}
+
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear()
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + " " + year;
+
+document.getElementById("getYear").textContent = year;
+document.getElementById("currentYear").textContent = fulldate;
+
