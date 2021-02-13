aba5b15f7927a3a5403e61d2189a0b1220b1505f
Matthew Felton <fel19007@byui.edu>
2021-02-12 23:11:42 -0600

    added lesson 6 and did images still need java

--- assets/js/lastupdated.js
+++ assets/js/lastupdated.js
@@ -1,3 +1,11 @@
 let lastUpdated = new Date(document.lastModified);
 
 document.getElementById("modified").textContent = "Last Updated: " + lastUpdated;
+
+WebFont.load({
+    google: {
+      families: [
+         ''
+      ]
+    }
+  });
