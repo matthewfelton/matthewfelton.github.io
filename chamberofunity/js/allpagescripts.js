083cd3593214b0bec0fa82050802a91bb112ce27
Matthew Felton <fel19007@byui.edu>
2021-03-27 17:20:32 -0500

    added chamber header

---
+++ chamberofunity/js/allpagescripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("disabled");
+    document.getElementById("primaryOpen").classList.toggle("disabled");
+    document.getElementById("primaryClose").classList.toggle("disabled");
+}
