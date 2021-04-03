9ec2156e02f6c19d7b2271d804e826c4fed2da19
Matthew Felton <fel19007@byui.edu>
2021-04-03 18:41:37 -0500

    added

---
+++ chamberofunity/js/directory.js
@@ -0,0 +1,46 @@
+const requestURL = 'json/directory.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+        //console.table(jsonObject);  
+        const businesses = jsonObject["businesses"];
+
+        for (let i = 0; i < businesses.length; i++) {
+
+            let card = document.createElement("section");
+
+            let image = document.createElement("img");
+            
+            image.setAttribute("src", businesses[i].logo);
+            image.setAttribute('loading', 'lazy');
+            image.setAttribute('alt', businesses[i].logo + " " + "logo");
+            
+            
+            let bInfo = document.createElement("div");
+            bInfo.classList.add("businessInfo");
+
+            let h2 = document.createElement("h2");
+            h2.textContent = businesses[i].businessName;
+            
+            let phone = document.createElement("p");
+            phone.textContent = "Phone: " + businesses[i].phoneNumber;
+            
+            
+            let web = document.createElement("p");
+            web.innerHTML = "Company Provided " + "<a href='" + businesses[i].website + "'>Website</a>";
+            
+            
+            card.appendChild(image);
+            bInfo.appendChild(h2);
+            bInfo.appendChild(phone);
+            bInfo.appendChild(web);
+            card.appendChild(bInfo);
+            
+        
+            document.querySelector('div.directoryGrid').appendChild(card);
+        }
+    
+    });
