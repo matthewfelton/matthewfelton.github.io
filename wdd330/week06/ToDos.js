760d97beb81aa252e748f4f322e4cc6c9c9e2437
matthewfelton <77081600+matthewfelton@users.noreply.github.com>
2021-10-23 03:09:47 -0500

    added many comments

--- wdd330/week06/ToDos.js
+++ wdd330/week06/ToDos.js
@@ -1,15 +1,18 @@
-//imports utility
+// Imports utility
 import { Utility } from "./utilities.js";
-//initializes utility
+// Initializes utility
 const todoUtility = new Utility();
+// Selects todo_list ul to assign display list 
 const todoList = document.querySelector(".todo_list");
+// Gets the tasks-left div and assigns it to taskTodo
 const taskTodo = document.getElementById("tasks-left");
-
+// Updates display after another function's action
 const updateTodoView = () => {
+    // Defines todoList innerHTML as blanks
     todoList.innerHTML = "";
-    //e = element i = index
+    // e = element i = index
     todoUtility.displayList.forEach((e, i) => {
-        //creates a li templete for todo list output
+        //creates a li templete for todo list output and assigns done or blank based on true or false of completed
         todoList.innerHTML += `<li class="todo_card ${e.completed ? "done" : ""}" id="${e.id}">
             <input type="checkbox" id="${e.id}check"/>
             <h3 class="todo_title">${e.content}</h3>
@@ -57,15 +60,17 @@ document.getElementById("add_todo").addEventListener("submit", (e) => {
     }
     updateTodoView();
 });
-
+// Listens for click of all button and applies view value of all
 document.getElementById("all-tasks-btn").addEventListener("click", () => {
     todoUtility.updateDisplayList('all');
     updateTodoView();
 });
+// Listens for click of all button and applies view value of false
 document.getElementById("active-tasks-btn").addEventListener("click", () => {
     todoUtility.updateDisplayList(false);
     updateTodoView();
 });
+// Listens for click of all button and applies view value of true
 document.getElementById("completed-tasks-btn").addEventListener("click", () => {
     todoUtility.updateDisplayList(true);
     updateTodoView();
