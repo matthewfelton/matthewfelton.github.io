91511a6fda6a1ee7d3bd2a7ab2915a2102b6db4c
matthewfelton <77081600+matthewfelton@users.noreply.github.com>
2021-12-12 00:47:51 -0600

    added lots of css

--- wdd330/pokedex/pokedex.js
+++ wdd330/pokedex/pokedex.js
@@ -6,21 +6,20 @@ const pokemon = new pokemonjs();
 const monList = document.querySelector(".mon_list")
 let fullPokemonList = {};
 let currentList = 'full';
+let bottomNumber = 12;
 
 pokemon.getInitialPokemonList().then((res) => {
     monList.innerHTML = "";
     fullPokemonList = res;
     if (fullPokemonList && Object.keys(fullPokemonList).length != 0) {
-        createList(Object.keys(fullPokemonList));
+        createList(Object.keys(fullPokemonList).slice(0, bottomNumber));
     } else {
         monList.innerHTML = "ERROR: Pokémon have escaped Dex... Beware Jurassic Pokémon Park"
     }
 }).finally((e) => {
-    addEventListenerToPokemon(Object.keys(fullPokemonList));
+    addEventListenerToPokemon(Object.keys(fullPokemonList).slice(0, bottomNumber));
     pokemon.getRestOfPokemon(fullPokemonList).then((res) => {
-        createList(Object.keys(res).slice(151));
         fullPokemonList = res;
-        addEventListenerToPokemon(Object.keys(fullPokemonList));
     });
 })
 
@@ -29,8 +28,6 @@ function addEventListenerToPokemon(arr) {
         document
             .getElementById(`${fullPokemonList[e].id}check`)
             .addEventListener("click", () => {
-                document.getElementById(fullPokemonList[e].id).classList.toggle("collected");
-                document.getElementById(fullPokemonList[e].id).classList.toggle("uncollected");
                 fullPokemonList[e].collected = !fullPokemonList[e].collected;
                 ls.set('pokemonList', fullPokemonList);
         });
@@ -49,13 +46,15 @@ function addEventListenerToPokemon(arr) {
 function createList (arr) {
     arr.forEach((e) => {
         monList.innerHTML += 
-        `<li class="card ${fullPokemonList[e].collected ? "collected" : "uncollected"}" id="${fullPokemonList[e].id}">
-            <input type="checkbox" id="${fullPokemonList[e].id}check"/>
-            <p>${fullPokemonList[e].name[0].toUpperCase()}${fullPokemonList[e].name.slice(1)}</p>
-            <img class="type" src="images/${fullPokemonList[e].typeOne}.png" loading="lazy"/></img>
-            ${fullPokemonList[e].typeTwo ? `<img class='type' src='images/${fullPokemonList[e].typeTwo}.png' loading='lazy'/></img>` : ''}
-            <img id="${fullPokemonList[e].id}image" src='${fullPokemonList[e].shiny ? fullPokemonList[e].simage : fullPokemonList[e].image}' loading="lazy"></img>
-            <input type="checkbox" id="${fullPokemonList[e].id}shiny"/>
+        `<li class="card" id="${fullPokemonList[e].id}">
+            <input type="checkbox" class="checkOne" id="${fullPokemonList[e].id}check"></input>
+            <img class="ball_icon"src="images/pokeball.png"></img>
+            <input type="checkbox" class="checkTwo" id="${fullPokemonList[e].id}shiny"></input>
+            <img class="shiny_icon"src="images/shiny.png"></img>
+            <h2>${fullPokemonList[e].name[0].toUpperCase()}${fullPokemonList[e].name.slice(1)}</h2>
+            <img class="typeOne" src="images/${fullPokemonList[e].typeOne}.png" loading="lazy"/></img>
+            ${fullPokemonList[e].typeTwo ? `<img class='typeTwo' src='images/${fullPokemonList[e].typeTwo}.png' loading='lazy'/></img>` : ''}
+            <img class="sprites" id="${fullPokemonList[e].id}image" src='${fullPokemonList[e].shiny ? fullPokemonList[e].simage : fullPokemonList[e].image}' loading="lazy"></img>
         </li>
         `
     });
@@ -63,8 +62,9 @@ function createList (arr) {
 
 function allButton() {
     monList.innerHTML = "";
-    createList(Object.keys(fullPokemonList));
-    addEventListenerToPokemon(Object.keys(fullPokemonList));
+    const displayList = Object.keys(fullPokemonList);
+    createList(displayList);
+    addEventListenerToPokemon(displayList);
     currentList = 'full';
 }
 document.getElementById("allBtn").addEventListener("click", () => {
@@ -107,4 +107,29 @@ document.getElementById("refresh").addEventListener("click", () => {
             allButton();
             break;
     }
+})
+
+document.getElementById("next").addEventListener("click", () => {
+    monList.innerHTML = "";
+    let orgBottomNumber = bottomNumber;
+    bottomNumber += 12;
+    if (bottomNumber > 1118) {
+        orgBottomNumber = 1118 - 12;
+        bottomNumber = 1118;
+    }
+    const displayList = Object.keys(fullPokemonList).slice(orgBottomNumber, bottomNumber);
+    createList(displayList);
+    addEventListenerToPokemon(displayList);
+})
+
+document.getElementById("prev").addEventListener("click", () => {
+    monList.innerHTML = "";
+    const orgBottomNumber = bottomNumber - 24;
+    bottomNumber = bottomNumber - 12;
+    if (bottomNumber < 0) {
+        bottomNumber = 12;
+    }
+    const displayList = Object.keys(fullPokemonList).slice(orgBottomNumber, bottomNumber);
+    createList(displayList);
+    addEventListenerToPokemon(displayList);
 })
